f97bb46470e84fc29841a8dcbd938a92795c66c8
jiwon79 <19-079@ksa.hs.kr>
2024-01-17 00:53:09 +0900

    :lipstick: show whole DatePicker UI

--- dummy-date/src/plugin/plugin.ts
+++ dummy-date/src/plugin/plugin.ts
@@ -2,20 +2,11 @@ import { PluginNetwork } from "./PluginNetwork";
 
 async function bootstrap() {
   figma.ui.onmessage = PluginNetwork.onMessage;
-
-  if (figma.editorType === "figma") {
-    figma.showUI(__html__, {
-      width: 240,
-      height: 280,
-      title: "Dummy Date",
-    });
-  } else if (figma.editorType === "figjam") {
-    figma.showUI(__html__, {
-      width: 240,
-      height: 280,
-      title: "Dummy Date",
-    });
-  }
+  figma.showUI(__html__, {
+    width: 300,
+    height: 420,
+    title: "Dummy Date",
+  });
 }
 
 bootstrap();
